e7906474f27a987479adb1b93998b3eaa14d7026
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-11-12 16:32:53 +0200

    direct tests and build script changed

--- build.sh
+++ build.sh
@@ -2,17 +2,6 @@ echo "Updating..."
 apt-get install libmysqlcppconn-dev clang -y
 echo "compiling..."
 cd ruri
-clang++ -std=c++2a lz4.c *cpp BCrypt/*c -D LINUX  -pthread -lmysqlcppconn
-cd ..
-mv ruri/a.out ./
-mv ruri/config.json ./
-rm -rf ruri
-mv a.out ruri
-mkdir data
-cd data
-mkdir screenshots
-mkdir maps
-mkdir replays
-cd ..
+clang++ -std=c++2a lz4.c *cpp BCrypt/*c -D LINUX  -pthread -lmysqlcppconn -o ruri
 
 echo "Done now run ruri and setup config!"
