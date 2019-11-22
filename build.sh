fa470df81618a6ed21a2dc0bd66fb6b8c28eb8eb
KotypeyPyEdition <39100756+KotypeyPyEdition@users.noreply.github.com>
2019-11-22 12:40:28 +0200

    fixed build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ echo "Updating..."
 apt-get install libmysqlcppconn-dev clang -y
 echo "compiling..."
 cd ruri
-clang++ -std=c++2a lz4.c *cpp BCrypt/*c -D LINUX  -pthread -lmysqlcppconn -o ruri
+clang -std=c++2a lz4.c *cpp BCrypt/*c -D LINUX  -pthread -lmysqlcppconn -o ruri
 
 echo "Done now run ruri and setup config!"
