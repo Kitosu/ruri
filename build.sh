fb73598565c0741a2464fbc971a9c3c7bfcc6ae2
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-02-24 22:32:47 +0100

    This worked for me

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "Updating..."
 apt-get install libmysqlcppconn-dev clang -y
-echo "compiling..."
+echo "Compiling..."
 cd ruri
-clang -std=c++2a lz4.c *cpp BCrypt/*c -D LINUX  -pthread -lmysqlcppconn -o ruri
+clang++ -std=c++2a lz4.c *cpp BCrypt/*c -D LINUX  -pthread -lmysqlcppconn -o ruri
 
 echo "Done now run ruri and setup config!"
